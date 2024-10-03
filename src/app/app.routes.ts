24436b43cfeec9e754f08bb4e43828c2e2a9dcf7
anavicentine <ana.ribeiro389@etec.sp.gov.br>
2024-10-03 11:04:58 -0300

    Jogos - Criação de Rotas Pescaria e batalha - Ana

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,10 @@
 import { Routes } from '@angular/router';
 import { InicioComponent } from '../pages/inicio/inicio.component';
+import { PescariaComponent } from '../pages/jogos/pescaria/pescaria.component';
+import { BatalhaComponent } from '../pages/jogos/batalha/batalha.component';
 
 export const routes: Routes = [
     { path: '', component: InicioComponent },
+    { path: 'pescaria', component: PescariaComponent },
+    { path: 'batalha', component: BatalhaComponent },
 ];
