87724b557eeb996806ecc237f57703d8d1384ac0
gabrielgarcia05 <gabriel.silva3140@etec.sp.gov.br>
2024-10-17 18:58:44 -0300

    telas perfil

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -6,12 +6,17 @@ import { UnAuthGuard } from '../auth/UnAuth.guard';
 import { HomeComponent } from '../pages/home/home.component';
 import { MemoriaComponent } from '../pages/jogos/memoria/memoria.component';
 import { VelhaComponent } from '../pages/jogos/velha/velha.component';
+import { DadosComponent } from '../pages/perfil/dados/dados.component';
+import { EmailComponent } from '../pages/perfil/email/email.component';
+import { SenhaComponent } from '../pages/perfil/senha/senha.component';
 
 export const routes: Routes = [
-    { path: '', component: LoginComponent, canActivate: [UnAuthGuard] },
-    { path: 'cadastro', component: CadastroComponent, canActivate: [UnAuthGuard] },
-    { path: 'home', component: HomeComponent, canActivate: [AuthGuard] },
-    { path: 'jogo-memoria/:mode', component: MemoriaComponent, canActivate: [AuthGuard] },
-    { path: 'jogo-velha/:mode', component: VelhaComponent, canActivate: [AuthGuard] }
-
+  { path: '', component: LoginComponent, canActivate: [UnAuthGuard] },
+  {path: 'cadastro',component: CadastroComponent,},
+  { path: 'home', component: HomeComponent, canActivate: [AuthGuard] },
+  {path: 'jogo-memoria/:mode', component: MemoriaComponent, canActivate: [AuthGuard],},
+  {path: 'jogo-velha/:mode', component: VelhaComponent, canActivate: [AuthGuard],},
+  {path: 'perfil-dados', component: DadosComponent },
+  {path: 'email-dados', component: EmailComponent},
+  {path: 'senha-dados', component: SenhaComponent},
 ];
