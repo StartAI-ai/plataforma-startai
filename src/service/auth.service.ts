86e6268087b2d40b5116e735838c33ce453fb706
Giovana Rocha <giovanarocha109@gmail.com>
2024-11-12 11:13:19 -0300

    Correção Bug

--- src/service/auth.service.ts
+++ src/service/auth.service.ts
@@ -1,9 +1,9 @@
-import { Injectable } from '@angular/core';
+import { Inject, Injectable, PLATFORM_ID } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Router } from '@angular/router';
 import { BehaviorSubject, finalize, Observable, catchError } from 'rxjs';
 import { throwError } from 'rxjs';
-
+import { isPlatformBrowser } from '@angular/common';
 @Injectable({
   providedIn: 'root'
 })
@@ -12,7 +12,12 @@ export class AuthService {
   private loadingSubject = new BehaviorSubject<boolean>(false);
   loading$ = this.loadingSubject.asObservable();
 
-  constructor(private http: HttpClient, private router: Router) {}
+  // BehaviorSubject para controlar o estado de autenticação
+  private isAuthenticatedSubject = new BehaviorSubject<boolean>(this.isAuthenticated());
+  isAuthenticated$ = this.isAuthenticatedSubject.asObservable();
+
+
+  constructor(@Inject(PLATFORM_ID) private platformId: Object, private http: HttpClient, private router: Router) {}
 
   // Função de cadastro
   cadastro(nome: string, email: string, senha: string, dataNascimento: string, controle: number): Observable<any> {
@@ -65,12 +70,17 @@ export class AuthService {
 
   // Verifica se o usuário está autenticado
   isAuthenticated(): boolean {
-    return !!localStorage.getItem('userData'); // Ajuste conforme seu caso
+    if (isPlatformBrowser(this.platformId)) {
+      // Verifica se está no navegador antes de acessar localStorage
+      return !!localStorage.getItem('userData');
+    }
+    return false; // Caso contrário, assume que o usuário não está autenticado
   }
 
   // Logout do usuário
   logout(): void {
     localStorage.removeItem('userData'); // Remove os dados do usuário
+    this.isAuthenticatedSubject.next(false); // Atualiza o estado de autenticação
     this.router.navigate(['/login']); // Ajuste a rota conforme sua aplicação
   }
 
--- src/template/template.component.ts
+++ src/template/template.component.ts
@@ -1,51 +1,58 @@
-import { Component, HostListener, Inject, PLATFORM_ID } from '@angular/core';
+import { Component, HostListener, Inject, PLATFORM_ID, OnInit, OnDestroy } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NavbarComponent } from '../componentes/navbar/navbar.component';
 import { CommonModule, isPlatformBrowser } from '@angular/common';
 import { AuthService } from '../service/auth.service';
 import { LoadingComponent } from '../componentes/loading/loading.component';
 import { MemoriaComponent } from "../pages/jogos/memoria/memoria.component";
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-template',
   standalone: true,
   imports: [RouterOutlet, NavbarComponent, CommonModule, LoadingComponent, MemoriaComponent],
   templateUrl: './template.component.html',
-  styleUrl: './template.component.css'
+  styleUrl: './template.component.css' 
 })
-export class TemplateComponent {
+export class TemplateComponent implements OnInit, OnDestroy {
 
   isDesktop = true;
   isLoading = false;
   isLogado = false;
+  private authSubscription: Subscription;
 
-  constructor(private authService: AuthService,@Inject(PLATFORM_ID) private platformId: Object) {
-    this.authService.loading$.subscribe(isLoading => {
-      this.isLoading = isLoading;
+  constructor(private authService: AuthService, @Inject(PLATFORM_ID) private platformId: Object) {
+    this.authSubscription = this.authService.isAuthenticated$.subscribe(isAuthenticated => {
+      this.isLogado = isAuthenticated;
     });
 
+    // Assinaturas para o loading
     this.authService.loading$.subscribe(isLoading => {
       this.isLoading = isLoading;
     });
 
     if (isPlatformBrowser(this.platformId)) {
       this.checkScreenSize();
-      this.isLogado = this.authService.isAuthenticated();
+      
     }
-
-    
   }
-  
+
+  ngOnInit(): void {
+    if (isPlatformBrowser(this.platformId)) {
+      this.checkScreenSize();
+    }
+  }
+
+  ngOnDestroy(): void {
+    // Certifique-se de cancelar a inscrição no Observable para evitar vazamentos de memória
+    if (this.authSubscription) {
+      this.authSubscription.unsubscribe();
+    }
+  }
 
   @HostListener('window:resize', ['$event'])
   onResize(event: Event) {
     this.checkScreenSize();
-  }
-
-  ngOnInit() {
-    if (isPlatformBrowser(this.platformId)) {
-      this.checkScreenSize();
-    }
   }
 
   private checkScreenSize() {
